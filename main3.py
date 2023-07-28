e5df3d885db606c060f3572a8f5013839de775d5
ARushan@appline.ru <ARushan@appline.ru>
2023-07-28 18:05:45 +0300

    дошел до 5.1.1

--- main3.py
+++ main3.py
@@ -1,31 +1,7 @@
-import json
-
-# a = str({'name': 'John Doe', 'age': 30, 'address': {'street': '123 Main St', 'city': 'Anytown', 'state': 'CA', 'zip_code': '12345'}})
-# d = json.dumps(eval(a), indent=4)
-# print(d)
+def func(phrase):
+    print(phrase)
+    func(phrase)
 
 
-a = {
-    "name": "John Doe",
-    "age": 30,
-    "address": {
-        "street": "123 Main St",
-        "city": "Anytown",
-        "state": "CA",
-        "zip_code": "12345"
-    }
-}
-
-b = {
-    'name': 'John Doe',
-    'age': 30,
-    'address': {
-        'street': '123 Main St',
-        'city': 'Anytown',
-        'state': 'CA',
-        'zip_code': '12345'
-    }
-}
-
-assert a == b
-
+func('Это рекурсия, детка!')
+print(obj.double_x)
